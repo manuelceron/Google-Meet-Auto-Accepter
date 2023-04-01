fe4d2b0acbb39daad7cd68cf6fabcb40eddf37da
manuelceron <manuelceron1@gmail.com>
2023-04-01 17:09:44 -0500

    Admit in spanish

--- content.js
+++ content.js
@@ -1,6 +1,21 @@
 setInterval(function () {
     for (let span of document.getElementsByTagName("span")) {
-        if (span.innerHTML === "수락") {
+        if (span.innerHTML === "Ver todos") {
+            span.click();
+        }
+        if (span.innerHTML === "Permirir") {
+            span.click();
+        }
+        if (span.innerHTML === "Admitir") {
+            span.click();
+        }
+        if (span.innerHTML === "Admitir a todos") {
+            span.click();
+        }
+        if (span.innerHTML === "Ver todos") {
+            span.click();
+        }
+        if (span.innerHTML === "Permirir a todos") {
             span.click();
         }
     }
